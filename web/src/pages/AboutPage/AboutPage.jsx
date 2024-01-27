8bdc5e6cfb16d6601a280f282a47271e57a44f55
Munavvarh <munavvarh123@gmail.com>
2024-01-27 17:06:14 -0500

    Finished Chapter 1

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,16 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+
+        <p>
+          I created this site to learn and explore more about JS framework Redwood.
+        </p>
+    </>
+  )
+}
+
+export default AboutPage
